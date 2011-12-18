9e3990cc6e92d7445701c8a4997165a6a255066a
Kazufumi Ohtani <kzmiff@gmail.com>
2011-12-18 16:33:21 +0900

    Initial Commit

---
+++ CircleProgressBar/cpbAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  cpbAppDelegate.h
+//  CircleProgressBar
+//
+//  Created by 和史 大谷 on 11/12/18.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cpbAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CircleProgressBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CircleProgressBar
+//
+//  Created by 和史 大谷 on 11/12/18.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "cpbAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([cpbAppDelegate class]));
+    }
+}
