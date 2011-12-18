dae6b0c3a80ba9e522c354f525fb4bfc80739523
Kazufumi Ohtani <kzmiff@gmail.com>
2011-12-18 20:02:23 +0900

    change comments

--- CircleProgressBar/CircleProgressBarView.m
+++ CircleProgressBar/CircleProgressBarView.m
@@ -1,11 +1,3 @@
-//
-//  CircleProgressBarView.m
-//  CircleProgressBar
-//
-//  Created by 和史 大谷 on 11/12/18.
-//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
-//
-
 #import "CircleProgressBarView.h"
 
 static inline float radians(double degrees) { return degrees * M_PI / 180.0; }
--- CircleProgressBar/main.m
+++ CircleProgressBar/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  CircleProgressBar
-//
-//  Created by 和史 大谷 on 11/12/18.
-//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 #import "cpbAppDelegate.h"
